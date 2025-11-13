d6876c5acb6dc19d91effac98caf7989c59aa3d4
DanSmith888 <dan@mustardfire.com>
2025-11-13 10:57:02 +0800

    verbose logging for ld enabled

--- scripts/build/build_image_5_libopenjp2.sh
+++ scripts/build/build_image_5_libopenjp2.sh
@@ -71,6 +71,8 @@ elif [[ $OS = 'Linux' ]]; then
         -DCMAKE_LIBRARY_PATH:path="${OUTPUT_LIB}" -DCMAKE_INCLUDE_PATH:path="${OUTPUT_INCLUDE}" \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DCMAKE_DISABLE_FIND_PACKAGE_WebP:BOOL=ON \
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,--verbose" \
         ./
 fi
 
